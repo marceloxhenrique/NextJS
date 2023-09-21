fa56419c0628c7dae309803ce34a882a8665168c
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-09-21 10:01:55 +0200

    Update Login.tsx and Signup.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,36 +11,50 @@ export default function LoginPage() {
   });
 
   const onLogin = async () => {};
-  console.log(user);
-  return (
-    <div className="flex  flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
-      {/* <hr /> */}
 
-      <label htmlFor="email">Email</label>
-      <input
-        className="text-black"
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className="text-black"
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <button onClick={onLogin} className="">
-        Login here
-      </button>
-      <Link href={"/signup"}> Visit Signup</Link>
-    </div>
+  return (
+    <main className="flex  flex-col items-center justify-center min-h-screen py-2 bg-gray-800">
+      <section className="flex flex-col p-8  border border-cya-800 rounded-md ">
+        <h1 className="flex justify-center text-2xl pb-4">Login</h1>
+        <label className="label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="input"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={user.email}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+        />
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="input"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+        />
+        <button onClick={onLogin} className="button">
+          Login
+        </button>
+
+        <p className="flex flex-row  justify-center mt-6">
+          Don{"'"}t have an account?
+          <Link href={"/signup"} className="underline text-blue-500 mx-1">
+            Signup
+          </Link>
+        </p>
+      </section>
+    </main>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,43 +15,63 @@ export default function SignupPage() {
   const onSignup = async () => {};
   console.log(user);
   return (
-    <div className="flex  flex-col items-center justify-center min-h-screen py-2">
-      <h1>Signup</h1>
-      <hr />
-      <label htmlFor="username">Username</label>
-      <input
-        className="text-black"
-        type="text"
-        id="username"
-        name="username"
-        placeholder="Username"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        className="text-black"
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className="text-black"
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, [e.target.name]: e.target.value })}
-      />
-      <button onClick={onSignup} className="">
-        Signup here
-      </button>
-      <Link href={"/login"}> Visit login page</Link>
-    </div>
+    <main className="flex  flex-col items-center justify-center min-h-screen py-2 bg-gray-800">
+      <section className="flex flex-col p-8  border border-cya-800 rounded-md ">
+        <h1 className="flex justify-center text-2xl pb-4">Signup</h1>
+
+        <label className="label" htmlFor="username">
+          Username
+        </label>
+        <input
+          className="input"
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+          value={user.username}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+        />
+        <label className="label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="input"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={user.email}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+        />
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="input"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+        />
+        <button onClick={onSignup} className="button">
+          Signup here
+        </button>
+        <p className="flex flex-row  justify-center mt-6">
+          Already have an account?
+          <Link className="underline text-blue-500 mx-1" href={"/login"}>
+            {" "}
+            Login
+          </Link>
+        </p>
+      </section>
+    </main>
   );
 }
